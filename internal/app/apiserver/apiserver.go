c21ce2c12c3233a4cb6c7e55e4abf6cb6a9c8918
jm1x <igormarkin2003@yandex.ru>
2024-02-29 00:39:45 +0300

    Added database and user interface

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -2,6 +2,7 @@ package apiserver
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/jm1x/http-rest-api/internal/app/store"
 	"github.com/sirupsen/logrus"
 	"io"
 	"net/http"
@@ -11,6 +12,7 @@ type APIServer struct {
 	config *Config
 	logger *logrus.Logger
 	router *mux.Router
+	store  *store.Store
 }
 
 func New(config *Config) *APIServer {
@@ -27,6 +29,10 @@ func (s *APIServer) Start() error {
 	}
 
 	s.configureRouter()
+
+	if err := s.configureStore(); err != nil {
+		return err
+	}
 
 	s.logger.Info("Starting API server...")
 
@@ -47,8 +53,18 @@ func (s *APIServer) configureRouter() {
 	s.router.HandleFunc("/hello", s.handleHello())
 }
 
+func (s *APIServer) configureStore() error {
+	st := store.New(s.config.Store)
+
+	if err := st.Open(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (s *APIServer) handleHello() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Hello, World!")
+		_, _ = io.WriteString(w, "Hello, World!")
 	}
 }
